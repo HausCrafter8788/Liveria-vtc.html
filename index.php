8d9772d52cc5e13f2bd5f4e36034624ca4f613d6
HausCrafter8788 <101967716+HausCrafter8788@users.noreply.github.com>
2023-02-11 19:37:19 +0000

    lol

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!----======== CSS ======== -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/index.css">
     
     <!----===== Boxicons CSS ===== -->
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
@@ -114,6 +114,6 @@
         <div class="text">Dashboard Sidebar</div>
     </section>
 
-    <script src="css/index.css"></script>
+    <script src="js/index.js"></script>
 </body>
 </html>
